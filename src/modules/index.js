898fcfcc89ac9e8adaac00d2b2a9b10f90fbc43f
Lucho Giribone <lagiribone@gmail.com>
2019-04-19 23:17:37 -0300

    initial configs

---
+++ src/modules/index.js
@@ -0,0 +1,14 @@
+
+import React, { PureComponent, Fragment } from 'react';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <Fragment>
+        hello world calendar
+      </Fragment>
+    );
+  }
+}
+
+export default App;
